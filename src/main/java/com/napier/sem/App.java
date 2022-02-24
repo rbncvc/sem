4415db1ee51fa4f3ed271c1854322d1ce0ebbc75
Ruben Cavaco <40539752@live.napier.ac.uk>
2022-02-24 15:18:51 +0000

    Get all current salaries from the database Print list of salaries

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -27,7 +27,7 @@ public class App {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(30);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
@@ -131,6 +131,24 @@ public class App {
         }
     }
 
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
+
     public static void main(String[] args) {
         // Create new Application
         App a = new App();
@@ -143,6 +161,9 @@ public class App {
 
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
+
+        //
+        a.printSalaries(employees);
 
         // Disconnect from database
         a.disconnect();
